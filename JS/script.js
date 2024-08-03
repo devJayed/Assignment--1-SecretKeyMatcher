a6b3b3888f77dbed245afae8cc8166e0d78c5fea
devJayed <jayedhossain.dev@gmail.com>
2024-08-03 14:47:08 +0600

    Right part, Enter pin section

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 let secreteKey = "";
 function generateSecretKey() {
+  secreteKey = "";
   const length = 6;
   const charset = "0123456789";
   for (let i = 0; i < length; i++) {
@@ -10,3 +11,43 @@ function generateSecretKey() {
   //   console.log(secreteKey);
 }
 // console.log(secreteKey);
+
+// Right sides
+const display = document.getElementById("result");
+let expression = "";
+
+function addNumber(num) {
+  if (expression.length >= 6) {
+    return;
+  }
+  expression += num;
+  display.innerHTML = expression;
+  //   console.log(num);
+}
+
+// Just delete last digit
+function clearLastDigit() {
+  expression = expression.slice(0, -1); // for -1, count from last digit
+  // console.log(typeof expression);
+  display.innerHTML = expression;
+}
+
+function clearDisplay() {
+  expression = "";
+  display.innerHTML = "0";
+}
+// match function of the two string
+function match() {
+  let matchId = document.getElementById("match");
+  let unmatchId = document.getElementById("unmatch");
+  matchId.innerText = "";
+  unmatchId.innerText = "";
+  //   console.log(secreteKey);
+  //   console.log(expression);
+  if (secreteKey == expression) {
+    // console.log("Matched");
+    matchId.innerText = "Your Secret Key is Matched";
+  } else {
+    unmatchId.innerText = "Your Secret Key is not Matched";
+  }
+}
